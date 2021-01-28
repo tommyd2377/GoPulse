ed0bbab67c32eebc14c70857153b6dc1bf892774
Tom DeVito <tdevito@icloud.com>
2021-01-28 16:53:37 -0500

    onUserDelete() delete stripe customer

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,8 +6,7 @@ const db = admin.firestore();
 const stripe = require('stripe')(functions.config().stripe.secret);
 
 export const createStripeCustomer = functions.auth
-  .user()
-  .onCreate(async (userRecord, context) => {
+  .user().onCreate(async (userRecord) => {
     const firebaseUID = userRecord.uid;
 
     const customer = await stripe.customers.create({
@@ -26,9 +25,20 @@ export const createStripeCustomer = functions.auth
       days_until_due: 30,
     })
 
-    return db.doc(`customers/${firebaseUID}`).update({
+    return db.doc("customers/" + firebaseUID).update({
       stripeId: customer.id,
       status: sub.status,
       subId: sub.id
     });
-  })
+  });
+
+  export const onUserDeleted = functions.auth.user().onDelete(async (user) => {
+    
+    const customer = ( await db.collection('customers').doc(user.uid).get()).data();
+    
+      if (customer) {
+        await stripe.customers.del(customer.stripeId);
+      }
+
+    return db.doc("customers/" + user.uid).delete();
+  });
